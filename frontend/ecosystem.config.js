08f121a65c214de723131d391189f9481cf0ace3
Софья Микуленко <nephilim45@yandex.ru>
2026-01-14 21:00:17 +1000

    add

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Smikulenko/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
