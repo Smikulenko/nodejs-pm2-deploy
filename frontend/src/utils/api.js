5ec189059388847280f7c2e926a9afe12c4be671
sofya Mikulenko <nephilim45@yandex.ru>
2026-01-15 19:20:05 +1000

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.mestosmiku.nomorepartiessbs.ru/');
+  const api = new Api('https://api.mestosmiku.nomorepartiessbs.ru');
   
   export default api;
   
